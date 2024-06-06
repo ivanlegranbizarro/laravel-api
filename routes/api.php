2e34d7e5250f7829a57278cd9bd31e74ddc793d0
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-06 19:42:02 +0200

    Modelo de Tasks

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 })->middleware('auth:api');
+
+
+Route::apiResource('tasks', TaskController::class);
