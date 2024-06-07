3fc71a144fb4c933d7a089a1af0cb55a3c0da6b8
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-07 09:26:21 +0200

    Unique en el UpdateSore

--- app/Http/Requests/UpdateTaskRequest.php
+++ app/Http/Requests/UpdateTaskRequest.php
@@ -11,7 +11,7 @@ class UpdateTaskRequest extends FormRequest
    */
   public function authorize(): bool
   {
-    return false;
+    return true;
   }
 
   /**
@@ -22,7 +22,7 @@ class UpdateTaskRequest extends FormRequest
   public function rules(): array
   {
     return [
-      'title' => 'string|min:3|max:255',
+      'title' => 'string|min:3|max:255|unique:tasks',
       'is_completed' => 'boolean',
     ];
   }
