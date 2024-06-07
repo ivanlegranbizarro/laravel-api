c7f1678bd7f496940db09c087cc1f4667e547379
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-07 08:56:09 +0200

    Customizando el mensaje de  error 404

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,17 +3,20 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+  ->withRouting(
+    web: __DIR__ . '/../routes/web.php',
+    api: __DIR__ . '/../routes/api.php',
+    commands: __DIR__ . '/../routes/console.php',
+    health: '/up',
+  )
+  ->withMiddleware(function (Middleware $middleware) {
+    //
+  })
+  ->withExceptions(function (Exceptions $exceptions) {
+    $exceptions->render(function (NotFoundHttpException $exception) {
+      return response()->json(['message' => 'This resource does not exist'], 404);
+    });
+  })->create();
