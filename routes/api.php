16a8fc5d82f9f417674b2448f33f9a9c82dea964
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-08 23:08:58 +0200

    policies y rutas arregladas
    He arreglado los policies y las rutas

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,9 @@
 
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-  return $request->user();
-})->middleware('auth:api');
 
 Route::post('/register', [UserController::class, 'register'])->name('register');
 Route::post('/login', [UserController::class, 'login'])->name('login');
 
-Route::apiResource('tasks', TaskController::class);
+Route::apiResource('tasks', TaskController::class)->middleware('auth:api');
