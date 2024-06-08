c12801bd28bc8f91936b694cd4f10984118b4a1f
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-08 08:51:58 +0200

    register and login

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+  public function register(StoreUserRequest $request): JsonResponse
+  {
+    $data = $request->validated();
+    $data['password'] = bcrypt($data['password']);
+    $user = User::create($data);
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json(['message' => 'User created', 'token' => $token], 201);
+  }
+
+  public function login(LoginUserRequest $request): JsonResponse
+  {
+    $data = $request->validated();
+    if (!auth()->attempt($data)) {
+      return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    $user = auth()->user();
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json(['token' => $token], 200);
+  }
+}
