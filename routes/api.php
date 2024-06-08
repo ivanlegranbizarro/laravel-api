3ff9d35a93fbeb3981a257513adc9e93c9ad064b
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-06-08 08:53:13 +0200

    rutas para el login y el register

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,5 +9,7 @@ Route::get('/user', function (Request $request) {
   return $request->user();
 })->middleware('auth:api');
 
+Route::post('/register', [UserController::class, 'register'])->name('register');
+Route::post('/login', [UserController::class, 'login'])->name('login');
 
 Route::apiResource('tasks', TaskController::class);
